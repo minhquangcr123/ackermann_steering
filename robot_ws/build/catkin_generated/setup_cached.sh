72ec5bf68ba1074183177830b9ad9a2e1897515f
Tran Minh Quang <minhquangcr123@gmail.com>
2019-12-09 23:00:47 +0700

    Change something

--- robot_ws/build/catkin_generated/setup_cached.sh
+++ robot_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
-export PWD="/home/tran/robot_ws/build"
+export CMAKE_PREFIX_PATH="/home/tran/github/robot_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/tran/github/robot_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/tran/github/robot_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/tran/github/robot_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/tran/github/robot_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/tran/robot_ws/src:/home/tran/github/robot_ws/src:$ROS_PACKAGE_PATH"
